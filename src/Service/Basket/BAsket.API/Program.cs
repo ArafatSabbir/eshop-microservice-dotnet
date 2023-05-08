85ac021b99875b6710b5538c5cdbecd7baa08dd0
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2023-05-09 00:29:12 +0600

    feature: working basket API project.

--- src/Service/Basket/BAsket.API/Program.cs
+++ src/Service/Basket/BAsket.API/Program.cs
@@ -1,11 +1,21 @@
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// add redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//inject dependencies
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
