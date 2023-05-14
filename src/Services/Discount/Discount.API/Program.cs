3b83210fe1b4cea782f613cbab9b6e3ed807d021
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2023-05-15 01:46:33 +0600

    Discount api using postgres with docker support.

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,6 +1,10 @@
+using Discount.API.Repositories;
+using Discount.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +12,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// call MigrateDatabase extension method
+app.MigrateDatabase<Program>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
