d1f9ebcdb85d568995b985678a19cdb2cfc9d008
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2023-07-04 00:36:05 +0600

    Add commands, exceptions and validators

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+{
+    private readonly ILogger<DeleteOrderCommandHandler> _logger;
+    private readonly IMapper _mapper;
+    private readonly IOrderRepository _orderRepository;
+
+    public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper,
+        ILogger<DeleteOrderCommandHandler> logger)
+    {
+        _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+        if (orderToDelete == null) throw new NotFoundException(nameof(Order), request.Id);
+
+        await _orderRepository.DeleteAsync(orderToDelete);
+
+        _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+
+        return Unit.Value;
+    }
+}
--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using Ordering.Domain.Entities;
 using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 
 namespace Ordering.Application.Mappings;
@@ -8,7 +10,7 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Order, OrdersVm>().ReverseMap();
-        // CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
-        // CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        CreateMap<Order, UpdateOrderCommand>().ReverseMap();
     }
 }
