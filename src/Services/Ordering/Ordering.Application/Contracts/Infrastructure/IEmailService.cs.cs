23c56111c202e2510007f48a776b1d06df5fffc9
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2023-07-03 23:36:13 +0600

    Add query for getting orderlist using CQRS pattern.

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs.cs
@@ -0,0 +1,7 @@
+﻿using Ordering.Application.Models;
+
+namespace Ordering.Application.Contracts.Infrastructure;
+public interface IEmailService
+{
+    Task<bool> SendEmail(Email email);
+}
---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Entities;
+using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+namespace Ordering.Application.Mappings;
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Order, OrdersVm>().ReverseMap();
+        // CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        // CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+    }
+}
