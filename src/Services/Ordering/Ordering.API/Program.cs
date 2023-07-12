6ef0140b114d7e0fa52822c093213e6eecd75ce4
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2023-07-13 02:57:23 +0600

    docker support for rabbitmq, Basket api and orering api.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,7 @@
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.OpenApi.Models;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
@@ -12,24 +15,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-// builder.Services.AddScoped<BasketCheckoutConsumer>();
+builder.Services.AddScoped<BasketCheckoutConsumer>();
 
 
-// // MassTransit Configuration
-// builder.Services.AddMassTransit(config =>
-// {
-//     config.AddConsumer<BasketCheckoutConsumer>();
-//     config.UsingRabbitMq((ctx, cfg) =>
-//     {
-//         cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
-//
-//         cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
-//         {
-//             c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
-//         });
-//     });
-// });
-// builder.Services.AddMassTransitHostedService();
+// MassTransit Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
